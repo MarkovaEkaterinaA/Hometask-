52daaae8be0fdd7c1b95931e77a8aae13afc486a
Ekaterina Markova <purtova2018@yandex.ru>
2024-02-23 17:03:40 +0300

    add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая принимает на вход число и 
+//проверяет, кратно ли оно одновременно 7 и 23.
+
+//161
+
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (161 % n == 0)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
