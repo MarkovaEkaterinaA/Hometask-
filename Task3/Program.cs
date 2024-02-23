17f59fb2858c79c74f09e36c3a4aba669781aba1
Ekaterina Markova <purtova2018@yandex.ru>
2024-02-23 17:04:46 +0300

    add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход целое число из 
+//отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int n = new Random().Next(10, 100); // [10, 99]
+Console.WriteLine(n);
+int n1 = n / 10;
+int n2 = n % 10;
+if (n1 > n2)
+    Console.WriteLine(n1);
+else
+    Console.WriteLine(n2);
